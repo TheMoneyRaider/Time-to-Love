67b1b623ed25ce4cc8e55e60ddca9457bbc15280
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2026-03-06 16:15:08 -0800

    More letter work

---
+++ Game Elements/ui/guide/letters/letter.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Letter
+
+# Exposed fields for editor
+@export var letter_id: int = -1
+@export var description: String = "What' the remnant doess in the letter"
--- Game Elements/ui/guide/letters/letter_menu.gd
+++ Game Elements/ui/guide/letters/letter_menu.gd
@@ -8,12 +8,24 @@ var current_focus_index := -2  # -1 means return focused
 @export var joystick_deadzone := 0.5  # adjust for your joystick sensitivity
 var last_input_dir := 0  # prevent repeated triggers
 var last_input_dirv := 0  # prevent repeated triggers
+var letter_pool: Array[Letter] = []
 
 var LayerManager : Node
 func _ready():
 	LayerManager = get_tree().get_root().get_node("LayerManager")
 	hide()
 
+func _load_all_letters() -> void:
+	#var dir = DirAccess.open("res://Game Elements/Remnants/")
+	var dir = ResourceLoader.list_directory("res://Game Elements/ui/guide/letters/")
+	if dir == null:
+		push_error("Letters folder not found: res://Game Elements/ui/guide/letters/")
+		return
+	for file in dir:
+		if file.ends_with(".tres"):
+			var res = ResourceLoader.load("res://Game Elements/ui/guide/letters/" + file)
+			if res:
+				letter_pool.append(res)
 
 
 func activate():
