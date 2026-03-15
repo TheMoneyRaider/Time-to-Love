82b4306accea7905b97e6c8cb4dd81746848419c
Zak Etterson <ettersoz@oregonstate.edu>
2026-03-15 11:08:50 -0700

    tress files in there

--- Game Elements/General Game/room_manager.gd
+++ Game Elements/General Game/room_manager.gd
@@ -23,7 +23,9 @@ var medieval_rooms : Array[Room] = [preload("res://Game Elements/Rooms/resources
 var western_rooms : Array[Room] = [preload("res://Game Elements/Rooms/resources/canyon1.tres"),
 								preload("res://Game Elements/Rooms/resources/canyon2.tres"),
 								preload("res://Game Elements/Rooms/resources/canyon3.tres"),
-								preload("res://Game Elements/Rooms/resources/canyon4.tres")]
+								preload("res://Game Elements/Rooms/resources/canyon4.tres"),
+								preload("res://Game Elements/Rooms/resources/canyon5.tres"),
+								preload("res://Game Elements/Rooms/resources/canyon6.tres")]
 
 var sci_fi_rooms : Array[Room] = [preload("res://Game Elements/Rooms/resources/factory1.tres"),
 								preload("res://Game Elements/Rooms/resources/factory2.tres"),
