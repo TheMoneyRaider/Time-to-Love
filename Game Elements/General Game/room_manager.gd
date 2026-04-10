42b49b78022c3ac22eb04ddd1f38df510dba3601
Sheparry <sheparry@oregonstate.edu>
2026-04-10 11:10:20 -0700

    Boss FIxes

--- Game Elements/General Game/room_manager.gd
+++ Game Elements/General Game/room_manager.gd
@@ -59,10 +59,11 @@ var sci_fi_shops : Array[Room] = [preload("res://Game Elements/Rooms/resources/s
 								
 var testing_room : Room = preload("res://Game Elements/Rooms/resources/testing_room.tres")
 #preload("res://Game Elements/Rooms/resources/testing_room.tres")
-var bosses : Array[Room] = [preload("res://Game Elements/Rooms/resources/scifi_boss.tres"),
+var bosses : Array[Room] = [preload("res://Game Elements/Rooms/resources/medieval_boss.tres"),
+	preload("res://Game Elements/Rooms/resources/scifi_boss.tres"),
 						preload("res://Game Elements/Rooms/resources/scifi_boss.tres"),
-						preload("res://Game Elements/Rooms/resources/scifi_boss.tres"),
-						preload("res://Game Elements/Rooms/resources/medieval_boss.tres")]
+						preload("res://Game Elements/Rooms/resources/scifi_boss.tres")
+						]
 
 var cached_scenes : Dictionary = {}
 
@@ -159,6 +160,6 @@ func update_ai_array(generated_room : Node2D, generated_room_data : Room, LayerM
 
 func get_boss_chance() -> float:
 	return pow((layer_ai[0]-10),2)/200 if current_progress-int(current_progress) > .85 else 0.0
-	
+	#WE NEED THIS TO BE QUICKER
 	
 	
