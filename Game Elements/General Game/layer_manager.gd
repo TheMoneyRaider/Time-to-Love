91b2f03a044d930b16b60a15914c91bd03d6a2e3
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2026-04-24 11:37:03 -0700

    Revert "Work on audio?"
    This reverts commit d20089a1ab2ff83ec38ad1be380c4f5f6a4e9766.

--- Game Elements/General Game/layer_manager.gd
+++ Game Elements/General Game/layer_manager.gd
@@ -4,7 +4,7 @@ extends Node2D
 ### Temp Multiplayer Fix
 var player1 = null
 var player2 = null
-var weapon1 = "res://Game Elements/Weapons/Railgun.tres"
+var weapon1 = "res://Game Elements/Weapons/Crossbow.tres"
 var weapon2 = "res://Game Elements/Weapons/Crowbar.tres"
 var undiscovered_weapons = []
 var possible_weapon = ""#undiscovered_weapons.pick_random()
