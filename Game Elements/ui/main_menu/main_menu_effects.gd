86e03e3b11e6ae1f33f7fdfa252b0fee7f77a3ca
ZE873 <ettersoz@oregonstate.edu>
2026-05-06 15:01:09 -0700

    MENU HOVER SFX
    i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation i hate menu fragmentation

--- Game Elements/ui/main_menu/main_menu_effects.gd
+++ Game Elements/ui/main_menu/main_menu_effects.gd
@@ -30,6 +30,8 @@ var UI: UIState = UIState.new()
 @onready var prev_state = null
 var paused : bool = true
 
+var hover_cooldown: float = 0.0
+
 var songs = [
 	preload("res://Game Elements/Music/western.wav"),
 	preload("res://Game Elements/Music/sci-fi.wav"),
@@ -52,6 +54,11 @@ func _ready():
 		UI_Group.get_node("VBoxContainer").get_child(2).grab_focus()
 		UI_Group.visible = true
 		Title.visible = true
+		
+		for button in $SubViewportContainer/SubViewport/UI_Group/VBoxContainer.get_children():
+			if button is Button:
+				button.mouse_entered.connect(_on_focus_entered)  # ← mouse hover
+				button.focus_entered.connect(_on_focus_entered)  # ← keyboard/controller
 	else:
 		if !capture_all_states:
 			preload_all_textures()
@@ -105,9 +112,10 @@ func start_menu_music():
 	var song_types = ["western", "sci-fi", "midieval"]
 	print("Playing: " + song_types[Globals.menu] + "\n" + str(songs[Globals.menu]))
 	var tween = create_tween()
-	tween.tween_property($AudioStreamPlayer, "volume_db", 0.0, 6.0)
+	tween.tween_property($AudioStreamPlayer, "volume_db", 0.0, 2.0)
 	
 func _process(delta):
+	hover_cooldown -=delta
 	$ColorRect.material.set_shader_parameter("time", $ColorRect.material.get_shader_parameter("time")+delta)
 	if paused:
 		if $Intro/AnimationPlayer.is_playing():
@@ -157,6 +165,9 @@ func _process(delta):
 		exploaded = false
 		cooldown = 1
 		rewind_ui(cooldown)
+		
+	
+	
 
 func fragment_disruption():
 	if get_viewport() and last_mouse_pos.distance_to(get_viewport().get_mouse_position()) >10:
@@ -413,6 +424,12 @@ func update_prompt():
 		text += button_state(Globals.player2_input,disruptive2)
 		$RichTextLabel.bbcode_text = text+": Toggle Fracturing "
 
+func _on_focus_entered() -> void:
+	print("focus entered")
+	if hover_cooldown <= 0.0:
+		print("playing audio")
+		$UIHover.play()
+		hover_cooldown = 0.1
 
 func button_state(input_type : String, active : bool):
 	if input_type == "key":
@@ -470,6 +487,15 @@ func update_ui_display():
 		"p2_press": UI.player2.pressing
 	})
 	if !prev_state or state!=prev_state:
+		
+		# handles UI sfx
+		if prev_state and (state["p1_hover"] != prev_state["p1_hover"] \
+		or state["p2_hover"] != prev_state["p2_hover"]):
+			if hover_cooldown <= 0.0:
+				print("playing hover sound")
+				$UIHover.play()
+				hover_cooldown = 0.1
+		
 		prev_state=state
 		var fname = generate_filename(prev_state)
 		if !ui_textures.has(fname):
