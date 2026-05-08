7440dcf47e80c9108dfce86e8c83ddee311e4a74
Sheparry <sheparry@oregonstate.edu>
2026-05-07 17:28:00 -0700

    fixed merge bug

--- Game Elements/Enemies/dynamEnemy.gd
+++ Game Elements/Enemies/dynamEnemy.gd
@@ -391,7 +391,7 @@ func _check_on_hit_remnants(dmg_owner: Node, attack_body: Node):
 					winter.remnant_name:
 						effect = preload("res://Game Elements/Effects/winter_freeze.tres").duplicate(true)
 						effect.cooldown = rem.variable_2_values[rem.rank-1] + .25 * mancer_value
-					  effect.value1 =  rem.variable_1_values[rem.rank-1] + 10 * mancer_value
+						effect.value1 =  rem.variable_1_values[rem.rank-1] + 10 * mancer_value
 						effect.gained(self)
 						effects.append(effect)
 					pyromancer.remnant_name:
