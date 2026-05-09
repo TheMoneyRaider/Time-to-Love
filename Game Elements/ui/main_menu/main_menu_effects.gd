9f9c49e7b37e504c04312350d5b4bc2de15e6c5d
ZE873 <ettersoz@oregonstate.edu>
2026-05-09 14:27:51 -0700

    new music manager for handling consistent music through changing scenes

--- Game Elements/ui/main_menu/main_menu_effects.gd
+++ Game Elements/ui/main_menu/main_menu_effects.gd
@@ -113,13 +113,10 @@ func start_menu_music():
 	$AudioStreamPlayer.stream = preload("res://Game Elements/Music/main_start.wav")
 	$AudioStreamPlayer.volume_db = 0.0
 	$AudioStreamPlayer.play()
-	print("playing start track")
 
 	$AudioStreamPlayer.finished.connect(func():
-		print("start finished, switching to loop")
 		$AudioStreamPlayer.stream = preload("res://Game Elements/Music/main_loop.wav")
 		$AudioStreamPlayer.play()
-		print("loop playing, stream: ", $AudioStreamPlayer.stream)
 	, CONNECT_ONE_SHOT)
 	
 	
