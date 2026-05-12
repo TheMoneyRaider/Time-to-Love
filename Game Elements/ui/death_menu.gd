66c4f80374e9864a471857a0142155985533d656
Sheparry <sheparry@oregonstate.edu>
2026-05-11 18:13:40 -0700

    Implemented alternative rewind
    Implemented a rewind effect that gets the framerate via regular prunings of the buffer. This keeps it so the buffer is a pretty much consistent size, and uses about 2.5 gb of ram rather than the 4 it was using before.

--- Game Elements/ui/death_menu.gd
+++ Game Elements/ui/death_menu.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 
 @export var time_per_buffer := 10 # hard cap on time will be this * 6
-@export var rewind_time := 10 #can't be smaller than recent_seconds. also the actual rewind time is generally 3 seconds or so greater.
+@export var rewind_time := 10.0 #can't be smaller than recent_seconds. also the actual rewind time is generally 3 seconds or so greater.
 
 @export var min_shader_intensity = 0.1
 @export	var max_shader_intensity = 1.25
@@ -21,6 +21,12 @@ var rewinding := false
 var total_time = 0.0
 var final_frame : Image
 
+var test_buffer := []
+var test_buffer_fps := 30
+var test_buffer_size = 600
+var test_frame_timer : float = 0.0
+var test_frame_duration : float = 1.0 / (test_buffer_fps * 2)
+
 var frame_amount = 0
 
 func _ready():
@@ -30,10 +36,11 @@ func _ready():
 		if button is Button:
 			button.disabled = true
 	capture_timer = Timer.new()
-	capture_timer.wait_time = 1.0 / buffer_fps[0]
+	capture_timer.wait_time = 1.0 / test_buffer_fps #buffer_fps[0]
 	capture_timer.one_shot = false
 	add_child(capture_timer)
-	capture_timer.timeout.connect(_capture_frame)
+	#capture_timer.timeout.connect(_capture_frame) TEST
+	capture_timer.timeout.connect(_test_capture_frame)
 	capture_timer.start()
 
 func _process(delta):
@@ -72,7 +79,7 @@ func _capture_frame():
 		return
 	var viewport = get_parent().get_node("game_container/game_viewport") as SubViewport
 	var img = viewport.get_texture().get_image()
-
+	img.compress(Image.CompressMode.COMPRESS_S3TC)
 	#Save final frame
 	if frame_amount == 3:
 		final_frame = img.duplicate(true)
@@ -84,6 +91,31 @@ func _capture_frame():
 			
 			if i < 5 and (frame_amount % (2 << i)) == 0:
 				buffers[i+1].append(oldest)
+
+func _test_capture_frame():
+	frame_amount +=1
+	if not capturing:
+		return
+	var viewport = get_parent().get_node("game_container/game_viewport") as SubViewport
+	var img = viewport.get_texture().get_image()
+	img.compress(Image.CompressMode.COMPRESS_S3TC)
+	#Save final frame
+	if frame_amount == 3:
+		final_frame = img.duplicate(true)
+	#Add to recent buffer (rotating)
+	test_buffer.append(img)
+	_resize_test_buffer()
+	
+func _resize_test_buffer():
+	var n = 8
+	if(test_buffer.size() >= test_buffer_size * (float(n) / (n-1))):
+		var new_buffer = []
+		for i in range(0, test_buffer.size() - (n-2), n):
+			for j in range(0, n - 1):
+				new_buffer.append(test_buffer[i+j])
+			
+		test_buffer = new_buffer
+		#test_buffer_fps.append(test_buffer_fps.back() * 2.0)
 
 func _on_quit_pressed():
 	if rewinding:
@@ -111,7 +143,8 @@ func _on_replay_pressed():
 	await tween.finished
 	
 	var now := Time.get_time_dict_from_system()
-	play_replay_reverse()
+	#play_replay_reverse() TEST
+	test_play_replay_reverse()
 
 func play_replay_reverse():
 	var reusable_texture := ImageTexture.new()
@@ -159,6 +192,31 @@ func play_replay_reverse():
 	
 	end_replay()
 
+func test_play_replay_reverse():
+	var reusable_texture := ImageTexture.new()
+	#Variables
+	var elapsed_frames = 0
+	var total_rewind_time = 0.0
+	total_rewind_time = test_buffer.size() / float(test_buffer_fps)
+
+	#Change rewind time if total time is too low
+	if total_rewind_time < rewind_time:
+		rewind_time = total_rewind_time
+	
+	while test_buffer.size() != 0:
+		await get_tree().process_frame
+		test_frame_timer += get_process_delta_time()
+		if(test_frame_timer >= test_frame_duration):
+			test_frame_timer -= test_frame_duration
+			reusable_texture.set_image(test_buffer.pop_back())
+			replay_texture.texture = reusable_texture
+			replay_texture.material.set_shader_parameter("intensity", get_shader_intensity(elapsed_frames / float(test_buffer_fps), rewind_time, min_shader_intensity, max_shader_intensity))
+			replay_texture.material.set_shader_parameter("time", elapsed_frames / float(test_buffer_fps))
+			elapsed_frames += 1
+	
+	end_replay()
+
+
 
 #this function calculates how blurry the screen is for the given frame
 func get_shader_intensity(current_time: float, total_time_func: float, min_intensity: float, max_intensity: float, exponent: float = 3.0) -> float:
@@ -173,8 +231,10 @@ func end_replay():
 	capturing = false
 	for i in range(6):
 		buffers[i].clear()
-		
 	frame_amount = 0
+	
+	test_buffer.clear()
+	test_frame_timer =0
 
 	# Create a full-screen overlay with the last frame
 	var overlay = preload("res://Game Elements/ui/transition_texture.tscn").instantiate()
