6b47183d53232462861bd2ed4bc6e52afc96fa57
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2026-05-15 09:12:01 -0700

    Canyon5 tilemap and z_indexs

--- Game Elements/General Game/room_manager.gd
+++ Game Elements/General Game/room_manager.gd
@@ -79,7 +79,7 @@ var normal_rooms : Array = []
 var shop_rooms : Array = []
 
 func get_room(room : Room):
-	return western_rooms[1]
+	return western_rooms[2]
 	var index = int(current_progress) if room.roomtype != Globals.RoomType.Boss else int(current_progress+1.0)
 	if index >= 3:
 		index = randi() % 3
