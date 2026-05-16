5f4effdcbda2cc78dbdfd8807091869fac95fb27
ZE873 <ettersoz@oregonstate.edu>
2026-05-16 11:06:40 -0700

    archer and skeleton have attack noises

--- Game Elements/General Game/room_manager.gd
+++ Game Elements/General Game/room_manager.gd
@@ -25,7 +25,7 @@ var layer_ai := [
 	0	#Rooms since shop room 			13
 	]
 #the root node of each room MUST BE NAMED Root
-@onready var current_progress = 3.0 #TEST 3.0
+@onready var current_progress = 0.0 #TEST 3.0
 var medieval_rooms : Array[Room] = [preload("res://Game Elements/Rooms/resources/cave1.tres"),
 								preload("res://Game Elements/Rooms/resources/cave2.tres"),
 								preload("res://Game Elements/Rooms/resources/cave3.tres"),
