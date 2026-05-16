ea04ff420321bb859c7418905a82c797240a4513
ZE873 <ettersoz@oregonstate.edu>
2026-05-16 00:51:39 -0700

    settings menu select sfx

--- Game Elements/ui/main_menu/main_menu.gd
+++ Game Elements/ui/main_menu/main_menu.gd
@@ -11,20 +11,25 @@ func _ready() -> void:
 		AudioServer.set_bus_volume_db(bus_index, volume)
 
 func _on_start_button_pressed() -> void:
+	sfx_manager.play(preload("res://Game Elements/ui/sfx/select_002.ogg"))
 	Globals.is_multiplayer = false
 	get_tree().call_deferred("change_scene_to_file", "res://Game Elements/General Game/layer_manager.tscn")
 
 func _on_start_m_button_pressed() -> void:
+	sfx_manager.play(preload("res://Game Elements/ui/sfx/select_002.ogg"))
 	Globals.is_multiplayer =true
 	get_tree().call_deferred("change_scene_to_file", "res://Game Elements/General Game/layer_manager.tscn")
 
 func _on_settings_button_pressed() -> void:
+	sfx_manager.play(preload("res://Game Elements/ui/sfx/select_002.ogg"))
 	get_tree().call_deferred("change_scene_to_file", "res://Game Elements/ui/settings.tscn")
 
 func _on_quit_button_pressed() -> void:
+	sfx_manager.play(preload("res://Game Elements/ui/sfx/select_002.ogg"))
 	get_tree().quit()
 
 
 func _on_save_state_button_pressed() -> void:
+	sfx_manager.play(preload("res://Game Elements/ui/sfx/select_002.ogg"))
 	print("save")
 	get_tree().call_deferred("change_scene_to_file", "res://Game Elements/ui/save_states/saves.tscn")
