e1a05d36dff7145aba30907a9696a65becf89419
ZE873 <ettersoz@oregonstate.edu>
2026-05-15 21:00:17 -0700

    sfx manager

---
+++ Game Elements/General Game/sfx_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func play(stream: AudioStream, volume_db: float = 0.0) -> void:
+	var player := AudioStreamPlayer.new()
+	player.stream = stream
+	player.bus = "SFX"
+	player.volume_db = volume_db
+	player.process_mode = Node.PROCESS_MODE_ALWAYS
+	add_child(player)
+	player.play()
+	player.finished.connect(player.queue_free)
