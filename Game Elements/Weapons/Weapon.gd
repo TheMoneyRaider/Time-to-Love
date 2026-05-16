47ea4ccb10979c17a24a4dd41fb5e05301c5f5de
ZE873 <ettersoz@oregonstate.edu>
2026-05-15 21:42:41 -0700

    weapon sfx implementation

--- Game Elements/Weapons/Weapon.gd
+++ Game Elements/Weapons/Weapon.gd
@@ -53,6 +53,14 @@ var special_start_damage : float = 1.0
 var special_started : bool = false
 var special_nodes = []
 
+var mace_sounds = [
+	preload("res://Game Elements/sfx/weapons/mace/mace1.ogg"),
+	preload("res://Game Elements/sfx/weapons/mace/mace2.ogg"),
+	preload("res://Game Elements/sfx/weapons/mace/mace3.ogg"),
+	preload("res://Game Elements/sfx/weapons/mace/mace4.ogg"),
+	preload("res://Game Elements/sfx/weapons/mace/mace5.ogg"),
+]
+
 static func create_weapon(resource_location : String, current_owner : Node2D):
 	var new_weapon = load(resource_location).duplicate(true)
 	var attack_instance = load(new_weapon.attack_scene).instantiate()
@@ -71,6 +79,24 @@ static func create_weapon(resource_location : String, current_owner : Node2D):
 
 func request_attacks(direction : Vector2, char_position : Vector2, node_attacking : Node, flip : int = 1):
 	#GAMBLER
+	
+	match type:
+		"Mace":
+			sfx_manager.play(mace_sounds[randi() % mace_sounds.size()])
+		#"Laser_Sword":
+			#sfx_manager.play(sword_sounds[randi() % sword_sounds.size()])
+		#"Crossbow":
+			#sfx_manager.play(crossbow_sounds[randi() % crossbow_sounds.size()])
+		#"Crowbar":
+			#sfx_manager.play(crowbar_sounds[randi() % crowbar_sounds.size()])
+		#"Railgun":
+			#sfx_manager.play(railgun_sounds[randi() % railgun_sounds.size()])
+		#"Shotgun":
+			#sfx_manager.play(shotgun_sounds[randi() % shotgun_sounds.size()])
+		#"RobotMelee":
+			#sfx_manager.play(robot_sounds[randi() % robot_sounds.size()])
+			
+			
 	var temp_spread = attack_spread
 	var temp_random = random_spread
 	if node_attacking.is_in_group("player"):
