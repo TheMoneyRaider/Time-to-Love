fcc4c9e3e9b2973b2b4c053733a115c27f0db514
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2026-05-16 17:13:03 -0700

    Reward and Pathway Icons are bigger

--- Game Elements/General Game/enemy_awareness.gd
+++ Game Elements/General Game/enemy_awareness.gd
@@ -82,6 +82,7 @@ func _process(_d):
 					glow.get_node("PathwayIcon1").visible = false
 					glow.get_node("PathwayIcon2").visible = false
 			1:
+				glow.scale *= 2.0
 				if version_changed:
 					glow.get_node("GlowCircle").visible = false
 					var reward = enemies[i].get_node("Image").texture
@@ -96,6 +97,7 @@ func _process(_d):
 					if(icon2.material != null):
 						icon2.material.set_shader_parameter("split", false)
 			2:
+				glow.scale *= 2.0
 				if version_changed:
 					glow.get_node("GlowCircle").visible = false
 					var pathway = enemies[i]
