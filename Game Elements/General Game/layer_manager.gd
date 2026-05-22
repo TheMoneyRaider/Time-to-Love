a1b475a5f61146412695bba9ad7d9d5766dd4ee8
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2026-05-21 17:28:48 -0700

    Aeromancer fix

--- Game Elements/General Game/layer_manager.gd
+++ Game Elements/General Game/layer_manager.gd
@@ -106,7 +106,7 @@ func _ready() -> void:
 	hud.set_players(player1,player2)
 	hud.connect_signals(player1)
 	hud.set_cross_position()
-	#dev_remnants()
+	dev_remnants()
 	
 	
 	
@@ -1987,12 +1987,18 @@ func _damage_indicator(damage : float, dmg_owner : Node,direction : Vector2 , at
 func dev_remnants():
 	var rem
 	
-	rem = load("res://Game Elements/Remnants/singularity.tres")
+	rem = load("res://Game Elements/Remnants/aeromancer.tres")
 	rem.rank = 5
 	player_1_remnants.append(rem.duplicate(true))
 	remnant_update(rem,player1,true)
 	player_2_remnants.append(rem.duplicate(true))
+	remnant_update(rem,player1,false)
+	rem = load("res://Game Elements/Remnants/mancermancer.tres")
+	rem.rank = 5
+	player_1_remnants.append(rem.duplicate(true))
 	remnant_update(rem,player1,true)
+	player_2_remnants.append(rem.duplicate(true))
+	remnant_update(rem,player1,false)
 	
 	#rem = load("res://Game Elements/Remnants/aeromancer.tres")
 	#rem.rank = 2
